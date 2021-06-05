90502ce3b965e37afc31949058b3e7a970879d1c
yuyu78 <ishiiyuki78@gmail.com>
2021-06-05 22:21:53 +0000

    Add code to increase one by one difficulty of the game

--- assets/js/startgame.js
+++ assets/js/startgame.js
@@ -3,7 +3,7 @@ function startGame() {
     document.getElementById('button-start').style.display = 'none';
 
     // Create a random list of number and add in the array
-    let turn = 6;
+    let turn = Number(document.getElementById("turn").innerHTML);
     numbers =[];
     for (i = 0; i < turn; i++) {
         let randomNumber = Math.floor(Math.random() * 9);
@@ -39,7 +39,9 @@ function playerFunctionality(turn) {
                 console.log(numbers);
                 if(numbers.length == 0) {
                     console.log("correct");
-                    // New turn 
+                    // New turn and increase difficulty one by one 
+                    turn = turn + 1;
+                    document.getElementById("turn").innerHTML = turn.toString();
                     document.getElementById("button-start").style.display = "block";
                 }
             } else {
