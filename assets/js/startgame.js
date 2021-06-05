71606e4382f90fc31b165df5512d1d478b42f0b1
yuyu78 <ishiiyuki78@gmail.com>
2021-06-05 16:30:47 +0000

    Add function showLights to display the sequence of light

--- assets/js/startgame.js
+++ assets/js/startgame.js
@@ -2,7 +2,7 @@ function startGame() {
     // When click on the button "Let's start, the button will disappear
     document.getElementById('button-start').style.display = 'none';
 
-    // Create a random number and add in the array
+    // Create a random list of number and add in the array
     let turn = 5;
     numbers =[];
     for (i = 0; i < turn; i++) {
@@ -10,4 +10,17 @@ function startGame() {
         numbers.push(randomNumber);
     }
     console.log(numbers);
+    showLights(0, turn)
+}
+
+function showLights(count, turn) {
+    document.getElementById(numbers[count].toString()).style.backgroundColor = "red";
+    i = count;
+    setTimeout(function() {
+        document.getElementById(numbers[i].toString()).style.backgroundColor = "black";
+        i++;
+        if (i < turn) {
+            showLights(i, turn);
+        }
+    }, 1000);
 }
