743478b99addaae1f7373fdcdb4b6e6e8d37cae0
yuyu78 <ishiiyuki78@gmail.com>
2021-06-05 15:58:05 +0000

    Create inside function startGame() a random list of number and add in the array

--- assets/js/startgame.js
+++ assets/js/startgame.js
@@ -1,3 +1,13 @@
 function startGame() {
+    // When click on the button "Let's start, the button will disappear
     document.getElementById('button-start').style.display = 'none';
+
+    // Create a random number and add in the array
+    let turn = 5;
+    numbers =[];
+    for (i = 0; i < turn; i++) {
+        let randomNumber = Math.floor(Math.random() * 9);
+        numbers.push(randomNumber);
+    }
+    console.log(numbers);
 }
