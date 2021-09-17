934ad1fc2a20008597253c0567fa745d2160fc64
yuyu78 <ishiiyuki78@gmail.com>
2021-09-17 11:10:02 +0000

    Add function to display highscoreboard

--- assets/js/scoreboards.js
+++ assets/js/scoreboards.js
@@ -1,3 +1,12 @@
+// Display the three highest score in the page score.html
+window.onload = getThreeHighScores;
 function getThreeHighScores() {
-    
+    let firstHighscore = JSON.parse(localStorage.getItem('highScore'))[2];
+    document.getElementById("first-highscore").innerHTML = firstHighscore;
+
+    let secondHighscore = JSON.parse(localStorage.getItem('highScore'))[1];
+    document.getElementById("second-highscore").innerHTML = secondHighscore;
+
+    let thirdHighscore = JSON.parse(localStorage.getItem('highScore'))[0];
+    document.getElementById("third-highscore").innerHTML = thirdHighscore;
 }
